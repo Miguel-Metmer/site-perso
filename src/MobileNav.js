2bb92f714369735c4586e690416197e6623551f1
Miguel-Metmer <Miguel.Metmer@outlook.com>
2020-09-16 20:51:16 +0200

    Ajout d'un menu mobile && correction taille police d'écriture

---
+++ src/MobileNav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./public/css/style.css";
+
+export default class MobileNav extends React.Component
+{
+    constructor()
+    {
+        super();
+    }
+    
+    render()
+    {
+        return(
+            <>
+                <nav>
+                    <ul className="mobile-menu">
+                        <li> <a href="#project-anchor">Projets</a> </li>
+                        <li> <a href="#">Technologies</a> </li>
+                        <li> <a href="#">Contact</a> </li> 
+                    </ul>
+                </nav>
+            </>
+        );
+    };
+};
--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,45 @@
 import React from "react";
+import MobileNav from "./MobileNav";
 import "./public/css/style.css";
 
 export default class Nav extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            isMobile : false
+        };
+    }
+
+    handleMenuClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            isMobile : !this.state.isMobile
+        })
+        console.log(this.state.isMobile);
+    }
+
     render()
     {
+        let mobile;
+        if(this.state.isMobile)
+        {
+            mobile = <MobileNav/>
+        }
+
         return(
             <>
                 <nav>
                     <h4> <a href="#">Miraevallis</a></h4>
-                    <ul>
+                    <ul className="desktop-menu">
                         <li> <a href="#project-anchor">Projets</a> </li>
                         {/* <li> <a href="#">Technologies</a> </li>
                         <li> <a href="#">Contact</a> </li> */}
                     </ul>
+
+                    <h5 onClick={this.handleMenuClick} className="mobile-burger"> <a href=""><i className="fas fa-bars"></i></a></h5>
+                    {mobile}
                 </nav>
             </>
         );
